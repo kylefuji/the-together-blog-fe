5dc931de9457bd7adaa5ced69787f436291e8c2b
kylefuji <kylefuji63@gmail.com>
2021-05-14 22:48:09 -0600

    navbar touch up and edited html to have icon and title changed

--- src/js/components/Navbar/Navbar.js
+++ src/js/components/Navbar/Navbar.js
@@ -2,15 +2,11 @@ import {DEV_URL, STATIC, IMAGES} from "../../constants/URLConstants.js"
 
 function Navbar() {
     return(
-        <div className="h-screen bg-purple-100 flex flex-col">
-            <div className="inline-flex">
-                <a href="#">
-                    <img className="h-8" src={DEV_URL + STATIC + IMAGES + "heart.svg"} alt=""></img>
-                </a>
-                <a href="#">
-                    The Together Blog
-                </a>
-            </div>
+        <div className="h-screen bg-purple-100 flex flex-col space-y-4">
+            <a href="#" className="inline-flex space-x-2">
+                <img src={DEV_URL + STATIC + IMAGES + "heart.svg"} alt=""></img>
+                <span className="text-xl">The Together Blog</span>
+            </a>
             <a href="#" class="text-indigo-600 hover:underline">Home</a>
             <a href="#" class="text-gray-600 hover:underline">Albums</a>
             <a href="#" class="text-gray-600 hover:underline">Posts</a>
